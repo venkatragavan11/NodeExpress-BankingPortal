7d9c80f489264d4b89a52c18ec6fdf5128f16159
venkatragavan11 <venkatragavan11@github.com>
2020-06-02 19:02:18 +0530

    Update in transfer

--- src/app.js
+++ src/app.js
@@ -66,8 +66,8 @@ app.get('/payment', (req, res) => {
 app.post('/payment', (req, res) =>  {
   accounts.credit.balance -= req.body.amount;
   accounts.credit.available += parseInt(req.body.amount, 10);
-  const json = JSON.stringify(accounts, null, 4);
-  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), json, 'utf8');
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
   res.render('payment', {message: 'Payment Successful' });
  });
 
