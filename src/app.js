34d049d383442325346ee6e38a65c692a6f48272
venkatragavan11 <venkatragavan11@github.com>
2020-06-02 16:42:28 +0530

    Error update in index file

--- src/app.js
+++ src/app.js
@@ -5,15 +5,15 @@ const express = require('express');
 
 const app = express();
 
-app.set('views', path.join(__dirname, views));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.render('index', {title: "Index"});
-}));
+    res.render('index', {title: 'Index'})
+});
 
 app.listen(3000, () => {
-  console.log("App running on port 3000 ")
+  console.log('App running on port 3000');
 })
